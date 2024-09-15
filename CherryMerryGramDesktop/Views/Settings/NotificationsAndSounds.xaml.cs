ec558e12c8c42bff725aae7071f313a5067da87e
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-09-16 00:48:23 +0300

    Privacy and Security settings design

--- CherryMerryGramDesktop/Views/Settings/NotificationsAndSounds.xaml.cs
+++ CherryMerryGramDesktop/Views/Settings/NotificationsAndSounds.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.UI.Xaml.Controls;
+using TdLib;
 
 namespace CherryMerryGramDesktop.Views.Settings;
 
 public partial class NotificationsAndSounds : Page
 {
+    private TdClient _client = App._client;
+    
     public NotificationsAndSounds()
     {
         InitializeComponent();
--- CherryMerryGramDesktop/Views/Settings/PrivacyAndSecurity.xaml.cs
+++ CherryMerryGramDesktop/Views/Settings/PrivacyAndSecurity.xaml.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.UI.Xaml.Controls;
+using TdLib;
 
 namespace CherryMerryGramDesktop.Views.Settings;
 
 public partial class PrivacyAndSecurity : Page
 {
+    private TdClient _client = App._client;
+    
     public PrivacyAndSecurity()
     {
         InitializeComponent();
+        
+        var blockedUsers = _client.ExecuteAsync(new TdApi.GetBlockedMessageSenders
+        {
+            BlockList = new TdApi.BlockList.BlockListMain(), Limit = 100
+        }).Result.TotalCount;
+        var connectedWebsites = _client.ExecuteAsync(new TdApi.GetConnectedWebsites()).Result.Websites.Length;
+        var activeSessions = _client.ExecuteAsync(new TdApi.GetActiveSessions()).Result.Sessions_.Length;
+        
+        CardBlockedUsers.Description = $"There are currently {blockedUsers} blocked users";
+        CardConnectedWebsites.Description = $"There are currently {connectedWebsites} connected websites";
+        CardActiveSessions.Description = $"There are currently {activeSessions} active sessions";
     }
 }
