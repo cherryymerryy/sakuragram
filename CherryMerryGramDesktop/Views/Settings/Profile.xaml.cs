1d8b888b87007d26260be8e923b3abc7045385ad
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-09-17 14:17:40 +0300

    Added BirthdayPicker

--- CherryMerryGramDesktop/Views/Settings/Profile.xaml.cs
+++ CherryMerryGramDesktop/Views/Settings/Profile.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Windows.Storage;
-using Windows.UI.Core;
 using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -34,7 +32,26 @@ public partial class Profile : Page
         TextBoxBio.Text = _currentUserFullInfo.Bio.Text;
         
         CardPhoneNumber.Description = $"Phone number: {_currentUser.PhoneNumber}";
-        CardDateOfBirth.Description = $"Date of birth: {_currentUserFullInfo.Birthdate.Day}.{_currentUserFullInfo.Birthdate.Month}.{_currentUserFullInfo.Birthdate.Year}";
+        
+        if (_currentUserFullInfo.Birthdate != null)
+        {
+            ButtonRemoveDateOfBirth.IsEnabled = true;
+            DatePicker.MaxYear = new DateTimeOffset(new DateTime(year: 2024, month: 12, day: 31));
+            DatePicker.MinYear = new DateTimeOffset(new DateTime(year: 1900, month: 1, day: 1));
+            DatePicker.SelectedDate = new DateTimeOffset(
+                new DateTime(
+                    _currentUserFullInfo.Birthdate.Year, 
+                    _currentUserFullInfo.Birthdate.Month, 
+                    _currentUserFullInfo.Birthdate.Day
+                ));
+            CardDateOfBirth.Description = 
+                $"Date of birth: {_currentUserFullInfo.Birthdate.Day}.{_currentUserFullInfo.Birthdate.Month}.{_currentUserFullInfo.Birthdate.Year}";
+        }
+        else
+        {
+            ButtonRemoveDateOfBirth.IsEnabled = false;
+            CardDateOfBirth.Description = "";
+        }
         
         UpdateProfilePhoto();
         
@@ -82,11 +99,78 @@ public partial class Profile : Page
             }).Result;
         }
     }
+
+    private void UpdateBirthDate()
+    {
+        _client.ExecuteAsync(new TdApi.SetBirthdate
+        {
+            Birthdate = new TdApi.Birthdate
+            {
+                Day = DatePicker.Date.Day,
+                Month = DatePicker.Date.Month,
+                Year = DatePicker.Date.Year
+            }
+        });
+        ButtonRemoveDateOfBirth.IsEnabled = true;
+        CardDateOfBirth.Description = 
+            $"Date of birth: {DatePicker.Date.Day}.{DatePicker.Date.Month}.{DatePicker.Date.Year}";
+    }
     
     private void ButtonLogOut_OnClick(object sender, RoutedEventArgs e)
     {
         _client.ExecuteAsync(new TdApi.LogOut());
         _client.CloseAsync();
         Application.Current.Exit();
+    }
+
+    private void ButtonSave_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (TextBoxFirstName.Text != _currentUser.FirstName || TextBoxLastName.Text != _currentUser.LastName)
+        {
+            _client.ExecuteAsync(new TdApi.SetName
+            {
+                FirstName = TextBoxFirstName.Text,
+                LastName = TextBoxLastName.Text
+            });
+        }
+        if (TextBoxUsername.Text != _currentUser.Usernames.EditableUsername)
+        {
+            _client.ExecuteAsync(new TdApi.SetUsername
+            {
+                Username = TextBoxUsername.Text
+            });
+        }
+        if (TextBoxBio.Text != _currentUserFullInfo.Bio.Text)
+        {
+            _client.ExecuteAsync(new TdApi.SetBio
+            {
+                Bio = TextBoxBio.Text
+            });
+        }
+
+        if (_currentUserFullInfo.Birthdate != null)
+        {
+            if (DatePicker.Date.Day != _currentUserFullInfo.Birthdate.Day ||
+                DatePicker.Date.Month != _currentUserFullInfo.Birthdate.Month ||
+                DatePicker.Date.Year != _currentUserFullInfo.Birthdate.Year)
+            {
+                UpdateBirthDate();
+            }
+        }
+        else
+        {
+            UpdateBirthDate();
+        }
+    }
+
+    private void ButtonRemoveDateOfBirth_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (_currentUserFullInfo.Birthdate == null) return;
+        _client.ExecuteAsync(new TdApi.SetBirthdate
+        {
+            Birthdate = null
+        });
+        ButtonRemoveDateOfBirth.IsEnabled = false;
+        DatePicker.SelectedDate = null;
     }
 }
