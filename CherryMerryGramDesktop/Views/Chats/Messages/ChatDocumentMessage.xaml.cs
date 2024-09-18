1825dc7ab8c879e90eb946f3eb89a708a248a6b1
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-09-19 00:17:23 +0300

    document and voice note message type

---
+++ CherryMerryGramDesktop/Views/Chats/Messages/ChatDocumentMessage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using TdLib;
+
+namespace CherryMerryGramDesktop.Views.Chats.Messages;
+
+public partial class ChatDocumentMessage : Page
+{
+    private static TdClient _client = App._client;
+    private TdApi.MessageContent.MessageDocument _messageDocument;
+    
+    public ChatDocumentMessage()
+    {
+        InitializeComponent();
+    }
+
+    public void UpdateMessage(TdApi.Message message)
+    {
+        switch (message.Content)
+        {
+            case TdApi.MessageContent.MessageDocument messageDocument:
+            {
+                _messageDocument = messageDocument;
+                DocumentName.Text = messageDocument.Document.FileName;
+                DocumentSize.Text = messageDocument.Document.Document_.Size.ToString();
+
+                if (messageDocument.Caption.Text != string.Empty)
+                {
+                    MessageCaptionText.Text = messageDocument.Caption.Text;
+                    MessageCaptionText.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    MessageCaptionText.Text = string.Empty;
+                    MessageCaptionText.Visibility = Visibility.Collapsed;
+                }
+                
+                break;
+            }
+        }
+    }
+
+    private void ButtonDownloadDocument_OnClick(object sender, RoutedEventArgs e)
+    {
+        _client.ExecuteAsync(new TdApi.DownloadFile
+        {
+            FileId = _messageDocument.Document.Document_.Id,
+            Priority = 1
+        });
+    }
+}
---
+++ CherryMerryGramDesktop/Views/Chats/Messages/ChatVoiceNoteMessage.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using TdLib;
+
+namespace CherryMerryGramDesktop.Views.Chats.Messages;
+
+public partial class ChatVoiceNoteMessage : Page
+{
+    private static TdClient _client = App._client;
+    private TdApi.MessageContent.MessageVoiceNote _messageVoiceNote;
+    private MediaPlayerElement _mediaPlayerElement;
+    private TimeSpan _position;
+    
+    public ChatVoiceNoteMessage()
+    {
+        InitializeComponent();
+        
+        _mediaPlayerElement = new MediaPlayerElement();
+        _client.UpdateReceived += async (_, update) => { await ProcessUpdate(update); };
+    }
+
+    private void MediaPlayerElement_MediaEnded(MediaPlayer sender, object args)
+    {
+        Icon.DispatcherQueue.TryEnqueue(DispatcherQueuePriority.Normal, () => Icon.Glyph = "\uE768");
+        _position = TimeSpan.Zero;
+    }
+
+    private async Task ProcessUpdate(TdApi.Update update)
+    {
+        switch (update)
+        {
+            case TdApi.Update.UpdateFile updateFile:
+            {
+                if (updateFile.File.Id == _messageVoiceNote.VoiceNote.Voice.Id)
+                {
+                    if (updateFile.File.Local.Path != string.Empty)
+                    {
+                        _mediaPlayerElement.DispatcherQueue.TryEnqueue(DispatcherQueuePriority.High, () => {
+                            PlayVoiceNote(updateFile.File.Local.Path);
+                        });
+                    }
+                    else if (_messageVoiceNote.VoiceNote.Voice.Local.Path != string.Empty)
+                    {
+                        _mediaPlayerElement.DispatcherQueue.TryEnqueue(DispatcherQueuePriority.High, () => {
+                            PlayVoiceNote(_messageVoiceNote.VoiceNote.Voice.Local.Path);
+                        });
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    public void UpdateMessage(TdApi.Message message)
+    {
+        switch (message.Content)
+        {
+            case TdApi.MessageContent.MessageVoiceNote messageVoiceNote:
+            {
+                _messageVoiceNote = messageVoiceNote;
+                VoiceNoteDuration.Text = messageVoiceNote.VoiceNote.Duration.ToString();
+
+                if (messageVoiceNote.Caption.Text != string.Empty)
+                {
+                    MessageCaptionText.Text = messageVoiceNote.Caption.Text;
+                    MessageCaptionText.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    MessageCaptionText.Text = string.Empty;
+                    MessageCaptionText.Visibility = Visibility.Collapsed;
+                }
+
+                if (messageVoiceNote.VoiceNote.Voice.Local.Path != string.Empty)
+                {
+                    Icon.Glyph = "\uE768";
+                    _mediaPlayerElement.Source = MediaSource.CreateFromUri(new Uri(messageVoiceNote.VoiceNote.Voice.Local.Path));
+                }
+                
+                break;
+            }
+        }
+    }
+
+    private void ButtonDownloadVoiceNote_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (_messageVoiceNote == null) return;
+        if (_messageVoiceNote.VoiceNote.Voice.Local.Path != string.Empty)
+        {
+            switch (_mediaPlayerElement.MediaPlayer.CurrentState)
+            {
+                case MediaPlayerState.Paused:
+                    PlayVoiceNote(_messageVoiceNote.VoiceNote.Voice.Local.Path);
+                    break;
+                case MediaPlayerState.Playing:
+                    PauseVoiceNote();
+                    break;
+            }
+        }
+        else
+        {
+            _client.ExecuteAsync(new TdApi.DownloadFile
+            {
+                FileId = _messageVoiceNote.VoiceNote.Voice.Id,
+                Priority = 1
+            });
+        }
+    }
+
+    private void PlayVoiceNote(string path)
+    {
+        _mediaPlayerElement.Source = MediaSource.CreateFromUri(new Uri(path));
+        if (_position != TimeSpan.Zero)
+        {
+            _mediaPlayerElement.MediaPlayer.Position = _position;
+        }
+        _mediaPlayerElement.MediaPlayer.Play();
+        _mediaPlayerElement.MediaPlayer.MediaEnded += MediaPlayerElement_MediaEnded;
+        Icon.Glyph = "\uE769";
+    }
+    
+    private void PauseVoiceNote()
+    {
+        _mediaPlayerElement.MediaPlayer.Pause();
+        _mediaPlayerElement.MediaPlayer.MediaEnded -= MediaPlayerElement_MediaEnded;
+        _position = _mediaPlayerElement.MediaPlayer.Position;
+        Icon.Glyph = "\uE768";
+    }
+}
