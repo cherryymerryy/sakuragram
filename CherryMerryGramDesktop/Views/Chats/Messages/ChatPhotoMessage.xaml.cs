bbf614cd35b6de2591e96e64e1e0acca4f4f2c07
醉樱桃 <72266338+cherryymerryy@users.noreply.github.com>
2024-09-19 13:45:12 +0300

    photo messages

---
+++ CherryMerryGramDesktop/Views/Chats/Messages/ChatPhotoMessage.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Media.Core;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using TdLib;
+
+namespace CherryMerryGramDesktop.Views.Chats.Messages;
+
+public partial class ChatPhotoMessage : Page
+{
+    private static TdClient _client = App._client;
+    private TdApi.MessageContent _messageMediaContent;
+    private int _profilePhotoFileId;
+    private int _mediaFileId;
+    
+    public ChatPhotoMessage()
+    {
+        InitializeComponent();
+        
+        _client.UpdateReceived += async (_, update) => { await ProcessUpdate(update); };
+    }
+
+    private async Task ProcessUpdate(TdApi.Update update)
+    {
+        switch (update)
+        {
+            case TdApi.Update.UpdateFile updateFile:
+            {
+                if (updateFile.File.Id == _mediaFileId)
+                {
+                    switch (_messageMediaContent)
+                    {
+                        case TdApi.MessageContent.MessagePhoto messagePhoto:
+                        {
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    public void UpdateMessage(TdApi.Message message)
+    {
+        _messageMediaContent = message.Content;
+        
+        var sender = message.SenderId switch
+        {
+            TdApi.MessageSender.MessageSenderUser u => u.UserId,
+            TdApi.MessageSender.MessageSenderChat c => c.ChatId,
+            _ => 0
+        };
+
+        if (sender > 0) // if senderId > 0 then it's a user
+        {
+            var user = _client.GetUserAsync(userId: sender).Result;
+            DisplayName.Text = user.FirstName + " " + user.LastName;
+            GetChatPhoto(user);
+        }
+        else // if senderId < 0 then it's a chat
+        {
+            var chat = _client.GetChatAsync(chatId: sender).Result;
+            DisplayName.Text = chat.Title;
+            ProfilePicture.Visibility = Visibility.Collapsed;
+        }
+        
+        try
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            dateTime = dateTime.AddSeconds(message.Date).ToLocalTime();
+            string sendTime = dateTime.ToShortTimeString();
+
+            SendTime.Text = sendTime;
+        } 
+        catch 
+        {
+            // ignored
+        }
+        
+        switch (_messageMediaContent)
+        {
+            case TdApi.MessageContent.MessagePhoto messagePhoto:
+            {
+                
+                break;
+            }
+        }
+    }
+    
+    private void GetChatPhoto(TdApi.User user)
+    {
+        if (user.ProfilePhoto == null)
+        {
+            ProfilePicture.DispatcherQueue.TryEnqueue(DispatcherQueuePriority.High, 
+                () => ProfilePicture.DisplayName = user.FirstName + " " + user.LastName);
+            return;
+        }
+        if (user.ProfilePhoto.Big.Local.Path != "")
+        {
+            try
+            {
+                ProfilePicture.DispatcherQueue.TryEnqueue(DispatcherQueuePriority.High,
+                    () => ProfilePicture.ProfilePicture = new BitmapImage(new Uri(user.ProfilePhoto.Big.Local.Path)));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        else
+        {
+            _profilePhotoFileId = user.ProfilePhoto.Big.Id;
+                
+            var file = _client.ExecuteAsync(new TdApi.DownloadFile
+            {
+                FileId = _profilePhotoFileId,
+                Priority = 1
+            }).Result;
+        }
+    }
+}
